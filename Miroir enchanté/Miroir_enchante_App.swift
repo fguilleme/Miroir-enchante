5f1990fa7d25eea46ef9e0a347a7f80285bc2749
François Guillemé <francois.guilleme@parrot.com>
2026-04-29 17:08:29 +0200

    Initial Commit

---
+++ Miroir enchanté/Miroir_enchante_App.swift
@@ -0,0 +1,17 @@
+//
+//  Miroir_enchante_App.swift
+//  Miroir enchanté
+//
+//  Created by François Guillemé on 29/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct Miroir_enchante_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
